35c39ed7fda4346c1eb9d706b76cc4a47294a6a2
PotatoKingTheVII <57728721+PotatoKingTheVII@users.noreply.github.com>
2019-11-23 19:53:11 +0000

    High density versions

---
+++ FilterHDDecoder.py
@@ -0,0 +1,65 @@
+import zlib
+from PIL import Image
+import numpy
+import re
+
+#User inputs:
+###################################
+filename = "Output.png"
+
+
+
+#Extract zlib from IDAT chunks
+###################################
+im = Image.open(filename)
+width, height = im.size
+with open(filename, 'rb') as fin:
+    data=fin.read() 
+    
+DatLocation = data.find(bytes("IDAT","utf-8"))
+CompressedZlibRaw = bytearray(data[DatLocation+4:len(data)])
+
+while(CompressedZlibRaw.find(bytes("IDAT","utf-8")) != -1):
+    DatLocationPlural = CompressedZlibRaw.find(bytes("IDAT","utf-8"))
+    CurrentCRCChunk = CompressedZlibRaw[DatLocationPlural-4:DatLocationPlural]
+    del CompressedZlibRaw[DatLocationPlural-8:DatLocationPlural+4]
+
+CompressedZlib  = CompressedZlibRaw[0:len(CompressedZlibRaw)-4]
+Decompressed = bytearray(zlib.decompress(CompressedZlib))
+
+
+
+#Extract data from filters
+###################################
+ByteArray = bytearray()
+
+if(len(im.getbands()) == 3): #Then it's an RGB only image
+    #print("RGB")
+    for i in range(0,len(Decompressed), ((width*3)+1)):
+        try:
+            ByteArray.append(int(Decompressed[i]))
+        except:
+            pass
+        
+elif(len(im.getbands())==1): #Is Palette image
+    #print("Plaette")
+    for i in range(0,len(Decompressed), (width*1)+1):
+        try:
+            ByteArray.append(int(Decompressed[i]))
+        except:
+            pass
+else: #Is RGBA image
+    #print("RGBA")
+    for i in range(0,len(Decompressed), (width*4)+1):
+        try:
+            ByteArray.append(int(Decompressed[i]))
+        except:
+            pass
+        
+FinalData = ByteArray
+
+with open("OutputRaw.raw", "wb") as fout:
+    fout.write(FinalData)
+    fout.close()
+
+print("Finished")
